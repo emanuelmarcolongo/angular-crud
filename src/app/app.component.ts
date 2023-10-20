b1af653fa7b3b42c4cfdc45085b1e7229e1e68cc
emanuelmarcolongo <emanuelmarcolongo@gmail.com>
2023-10-20 20:24:30 -0300

    feat: validate user login and change screen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-crud';
+  user?: any;
+  users?: any[];
+
+  createUser: any = {
+    name: 'Emanuel',
+    job: 'Dev',
+    birthDate: '1997-02-11T00:00:00.000Z',
+    email: 'manu@teste.com',
+    password: '123',
+    phone: '22-998669093',
+    adress: {
+      street: 'jabuti',
+      number: 200,
+      state: 'RJ',
+      city: 'Cabo Frio',
+      neighborhood: 'centro',
+      zipCode: '22222-22',
+    },
+  };
+
+  ngOnInit(): void {
+    this.users = JSON.parse(localStorage.getItem('Users') || '[]');
+
+    const authenticatedUser = JSON.parse(
+      localStorage.getItem('User') || 'null'
+    );
+
+    if (authenticatedUser) {
+      this.user = authenticatedUser;
+    }
+
+    this.users?.push(this.createUser);
+    localStorage.setItem('Users', JSON.stringify(this.users));
+  }
 
   fazerLogin(user: any) {
-    console.log(user);
+    const userExists: any | undefined = this.users?.find(
+      (u) => u.email === user.email && user.password === u.password
+    );
+
+    if (userExists) {
+      console.log('Usuário autenticado!', userExists);
+      this.user = userExists;
+      localStorage.setItem('User', JSON.stringify(this.user));
+    } else {
+      console.log('Usuário não autenticado!');
+    }
   }
 }
